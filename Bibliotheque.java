7f900e388ff44524e5eef95465beeba567679a13
timeoblrynov <timeo.baffreauleroux@ynov.com>
2026-04-30 10:25:00 +0200

    tg

--- Bibliotheque.java
+++ Bibliotheque.java
@@ -5,20 +5,41 @@ import java.util.*;
 public class Bibliotheque {
     private ArrayList<Book> bibliotheque = new ArrayList<>();
 
-    public ArrayList<String> returnBibliotheque() {
-        public ArrayList<Book> temp = new ArrayList<>();
-        for(Integer i = 0; i < bibliotheque.; i ++) {
-            add.temp(get_info());
+    public ArrayList getIds(){
+        ArrayList<Integer> list_ids = new ArrayList<>();
+        for (int i = 0; i < bibliotheque.size(); i ++){
+            list_ids.add(bibliotheque.get(i).getId());
         }
+        return list_ids;
+    }
+
+    public int getIdMax(ArrayList ids){
+        int id_max = 0;
+        []arr = new int [ids.size()];
+        ids.toArray(arr);
+        for (int i = 0; i < ids.size(); i ++){
+            if (ids.get(i)>id_max){
+                id_max = ids.get(i);
+            }
+        }
+        return id_max;
+    }
+
+    public ArrayList<String> returnBibliotheque() {
+        ArrayList<String> temp = new ArrayList<>();
+        for(int i = 0; i < bibliotheque.size(); i ++) {
+            temp.add(bibliotheque.get(i).get_info());
+        }
+        return temp;
     }
 
     public void addBook(title,author) {
-        id = book.getId();
+        id = getIdMax();
         livre = new Book{title,author,id};
         bibliotheque.add(book);
     }
 
-    public deletBook(book) {
+    public deletBook(id) {
         bibliotheque.remove(book);
     }
 }
